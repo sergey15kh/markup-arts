8b512f9dc71387e02a4178f6f431c4f29dc97ac1
Sergey <62428651+sergey15kh@users.noreply.github.com>
2021-08-10 19:52:00 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+// Testimonials Slider
+$(function() {
+    $('.testimonials-items').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed:3000,
+        speed:900,
+      });
+});
+
+// Burger Menu
+$(document).ready(function() {
+    $('.header__menu-burger').click(function(event) {
+        $('.burger-menu').toggleClass('active');
+    });
+});
+
+// ScrollBar
+$(function() {
+    $(".seo-block").mCustomScrollbar({
+        theme:"dark"
+    });
+});
